c2565014e7c8aeb2a0aa608adeceec5ced8e8cd2
Tarun <txrun2006@gmail.com>
2026-06-29 02:31:35 +0530

    week 2 progress

---
+++ Week2/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("BookRepository Bean Created");
+    }
+
+    public void display() {
+        System.out.println("Book Repository Working");
+    }
+}
