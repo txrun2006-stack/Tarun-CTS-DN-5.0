d7f88c8728b689c4e529db853afb9289df6939c6
Tarun <txrun2006@gmail.com>
2026-06-29 02:47:56 +0530

    week 2 progress

---
+++ Week2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("BookService Bean Created");
+    }
+
+    public void display() {
+        System.out.println("Book Service Working");
+    }
+}
